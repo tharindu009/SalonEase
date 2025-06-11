c6cafae6e1a159c5eb2ff148dab31ba0e9e55aa6
Harendra <tharindu009@hotmail.com>
2025-06-11 16:38:38 -0600

    add admin cors new

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,8 @@ connectCloudinary();
 app.use(express.json());
 const allowedOrigins = [
   'https://salon-ease.vercel.app',
+  'https://whitesmoke-echidna-955500.hostingersite.com/',
   'https://salon-ease-frontend.vercel.app/',
-  'https://salon-ease-admin.vercel.app/',
   'http://localhost:5173',
   'http://localhost:5174',
   'http://localhost:3000',
